83f0195b415ae45cf9e3a4e406a23228b569f72a
unknown <ballourk@gmail.com>
2023-04-02 16:03:56 +0300

    implemented navbar functionality

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,16 @@
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './Context';
+
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav className="nav-center">
+      <h3 className="logo">strapi</h3>
+      <button className="toggle-btn" onClick={openSidebar}>
+        <FaBars />
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
