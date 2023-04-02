d5339727812cc4a84f273e2776d0c3d88c2f6246
unknown <ballourk@gmail.com>
2023-04-02 17:18:34 +0300

    Link-items

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,19 @@
 import { FaBars } from 'react-icons/fa';
 import { useGlobalContext } from './Context';
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
   const { openSidebar } = useGlobalContext();
   return (
-    <nav className="nav-center">
-      <h3 className="logo">strapi</h3>
-      <button className="toggle-btn" onClick={openSidebar}>
-        <FaBars />
-      </button>
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/*Nav links*/}
+        <NavLinks />
+      </div>
     </nav>
   );
 };
