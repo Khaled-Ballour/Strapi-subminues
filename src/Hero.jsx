cc2aec72b6bf37bdb77ab5dcdec4b9798f50ee9a
unknown <ballourk@gmail.com>
2023-04-02 16:33:10 +0300

    sidebar

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,19 @@
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Mange Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+          expedita nobis cumque ut magnam debitis perspiciatis sit, voluptate
+          explicabo tempora vitae numquam quae ipsa animi incidunt voluptatum
+          facere, dolore molestiae.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
